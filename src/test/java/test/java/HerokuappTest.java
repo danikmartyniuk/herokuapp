265b781def6746fe8c42046b471b1470ad853f8f
danikmartyniuk <daniilmartyniuk.sbmt@gmail.com>
2019-12-26 23:27:29 +0300

    all tasks completed

--- src/test/java/test/java/HerokuappTest.java
+++ src/test/java/test/java/HerokuappTest.java
@@ -16,9 +16,8 @@ import static org.testng.Assert.assertEquals;
 public class HerokuappTest {
 
     @Test
-    public void addRemoveElements () {
+    public void addRemoveElements() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
         driver.findElement(By.tagName("button")).click();
@@ -30,7 +29,7 @@ public class HerokuappTest {
     }
 
     @Test
-    public void checkCheckboxes () {
+    public void checkCheckboxes() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/checkboxes");
@@ -44,29 +43,58 @@ public class HerokuappTest {
     }
 
     @Test
-    public void checkDropdown () {
+    public void checkDropdown() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/dropdown");
         Select options = new Select(driver.findElement(By.id("dropdown")));
         options.selectByVisibleText("Option 1");
-        options.deselectAll();
-        options.deselectByVisibleText("Option 2");
+        List<WebElement> option1 = options.getAllSelectedOptions();
+        String[] o1 = new String[option1.size()];
+        int i = 0;
+        for (WebElement a : option1) {
+            o1[i] = a.getText();
+            i++;
+        }
+        assertEquals(o1[0], "Option 1", "The other option is chosen");
+        options.selectByVisibleText("Option 2");
+        List<WebElement> option2 = options.getOptions();
+        String[] o2 = new String[option2.size()];
+        int g = 0;
+        for (WebElement a : option2) {
+            o2[g] = a.getText();
+            g++;
+        }
+        assertEquals(o2[0], "Option 2", "The other option is chosen");
     }
 
     @Test
-    public void checkHovers () {
+    public void checkHovers() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/hovers");
         Actions action = new Actions(driver);
-        WebElement img1 = driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/img"));
-        action.moveToElement(img1).moveToElement(driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div/a"))).click().build().perform();
+
+        WebElement img1 = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/img"));
+        action.moveToElement(img1).moveToElement(driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/a"))).click().build().perform();
+        String notFound1 = driver.findElement(By.xpath("/html/body/h1")).getText();
+        assertEquals(notFound1, "Not Found", "There is no error");
         driver.navigate().back();
+
+        WebElement img2 = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/img"));
+        action.moveToElement(img2).moveToElement(driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/a"))).click().build().perform();
+        String notFound2 = driver.findElement(By.xpath("/html/body/h1")).getText();
+        assertEquals(notFound2, "Not Found", "There is no error");
+        driver.navigate().back();
+
+        WebElement img3 = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[3]/img"));
+        action.moveToElement(img3).moveToElement(driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[3]/div/a"))).click().build().perform();
+        String notFound3 = driver.findElement(By.xpath("/html/body/h1")).getText();
+        assertEquals(notFound3, "Not Found", "There is no error");
     }
 
     @Test
-    public void arrowUpDown () {
+    public void arrowUpDown() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         Actions action = new Actions(driver);
@@ -83,7 +111,7 @@ public class HerokuappTest {
     }
 
     @Test
-    public void checkNotification () {
+    public void checkNotification() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/notification_message_rendered");
@@ -94,14 +122,7 @@ public class HerokuappTest {
     }
 
     @Test
-    public void checkTables () {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://the-internet.herokuapp.com/tables");
-    }
-
-    @Test
-    public void checkTypos () {
+    public void checkTypos() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/typos");
